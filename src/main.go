4566fe7460538af4d2f998912f81de70345e451a
nerdthatnoonelikes <kenrag24@gmail.com>
2021-04-21 09:56:39 -0500

    It's all coming together...

--- src/main.go
+++ src/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/nerdthatnoonelikes/follower/src/structs"
 )
 
 func main() {
 	client := &http.Client{}
 	user := os.Args[1]
-	followUser(user, client)
+	// followUser(user, client)
+	GetFollowers(user, client)
 }
 
 func followUser(username string, client *http.Client) {
@@ -24,4 +29,22 @@ func followUser(username string, client *http.Client) {
 	if err != nil {
 		fmt.Println("An error occured")
 	}
+}
+
+func GetFollowers(username string, client *http.Client) structs.UsersFollowers {
+	req, _ := http.NewRequest("GET", "https://api.github.com/users/"+username+"/followers", nil)
+	req.Header.Set("Authorization", "token "+"tokenhere")
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("An error occured")
+	}
+
+	var response structs.UsersFollowers
+	bodyBytes, _ := ioutil.ReadAll(res.Body)
+
+	json.Unmarshal(bodyBytes, &response)
+
+	return response
 }
