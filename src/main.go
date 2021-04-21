f215446fb91edc6ee09da621d1e01e020ffb74be
nerdthatnoonelikes <kenrag24@gmail.com>
2021-04-21 09:07:45 -0500

    add FollowUser function

--- src/main.go
+++ src/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	client := &http.Client{}
+	user := os.Args[1]
+	followUser(user, client)
+}
+
+func followUser(username string, client *http.Client) {
+	// Make a PUT request
+	req, _ := http.NewRequest("PUT", "https://api.github.com/user/following/"+username, nil)
+	req.Header.Set("Authorization", "token "+"tokenhere")
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+
+	_, err := client.Do(req)
+
+	// Check if an error occured
+	if err != nil {
+		fmt.Println("An error occured")
+	}
 }
